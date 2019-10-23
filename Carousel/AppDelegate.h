78cdd530cc987b0d725ead840febbe91a5ae159d
ikeek <ikeekcn@gmail.com>
2019-10-23 20:09:58 +0800

    Initial Commit

---
+++ Carousel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Carousel
+//
+//  Created by Zhuang Yang on 2019/10/23.
+//  Copyright © 2019 Zhuang Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Carousel/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Carousel
+//
+//  Created by Zhuang Yang on 2019/10/23.
+//  Copyright © 2019 Zhuang Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
